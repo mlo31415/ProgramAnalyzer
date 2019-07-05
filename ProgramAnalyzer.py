8d5c508095bbab93839e6a90bc69f39a6140e619
mlo31415 <mlo@baskerville.org>
2019-07-04 21:06:05 -0400

    Some improvements in the HTML

--- ProgramAnalyzer.py
+++ ProgramAnalyzer.py
@@ -581,7 +581,7 @@ for time in gTimes:
     if d != day:
         # Close the old file, if any
         if f is not None:
-            f.write('</table>\n')
+            f.write('</font></table>\n')
             # Read and append the footer
             with open("control-WebpageFooter.txt", "r") as f2:
                 f.writelines(f2.readlines())
@@ -596,6 +596,7 @@ for time in gTimes:
             f.writelines(f2.readlines())
         f.write("<h2>"+day+"</h2>\n")
         f.write('<table border="0" cellspacing="0" cellpadding="2">\n')
+        f.write('<font face="Calibri">\n')
 
     f.write('<tr><td colspan="3">')
     f.write('<b><span style="font-size: 14pt">' + NumericToTextTime(time) + '</span></b>')
@@ -604,15 +605,15 @@ for time in gTimes:
         # Now search for the program item and people list for this slot
         for itemName, item in gItems.items():
             if item.Time == time and item.Room == room:
-                f.write('<tr><td width="40">&nbsp;</td><td colspan="2">')
-                f.write('<p style="font-size: 12pt"><i>' + room +': </i></span><span style="font-size: 12pt"><b>' + item.DisplayName +'</b></span>')
+                f.write('<tr><td width="40">&nbsp;</td><td colspan="2">')   # Two columns, the first 40 pixes wide and empty
+                f.write('<p style="font-size: 12pt"><i>' + room +': </i><b>' + item.DisplayName +'</b></span>')
                 f.write('</td></tr>')
                 if item.People is not None and len(item.People) > 0:            # And the item's people list
-                    f.write('<tr><td width="40">&nbsp;</td><td width="40">&nbsp;</td><td width="600">')
+                    f.write('<tr><td width="40">&nbsp;</td><td width="40">&nbsp;</td><td width="600">')     # Three columns, the first two 40 pixes wide and empty; the third 600 pixels wide
                     f.write('<p style=font-size: 12pt">'+item.DisplayPlist()+'</span>')
                     f.write('</td></tr>\n')
                 if item.Precis is not None:
-                    f.write('<tr><td width="40">&nbsp;</td><td width="40">&nbsp;</td><td width="600">')
+                    f.write('<tr><td width="40">&nbsp;</td><td width="40">&nbsp;</td><td width="600">')     # Same
                     f.write('<p style=font-size: 10pt"><i>'+item.Precis+'</span></i>')
                     f.write('</td></tr>\n')
 if f is not None:
