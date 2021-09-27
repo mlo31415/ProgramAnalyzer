1ef2ddea1b1c750736d950873178726a27420080
mlo31415 <mlo@baskerville.org>
2021-09-27 13:55:39 -0400

    Correct typo

--- ProgramAnalyzer.py
+++ ProgramAnalyzer.py
@@ -315,7 +315,7 @@ def main():
     # Check for people in the schedule who are not in the people tab
     fname=os.path.join( reportsdir, "Diag - People in schedule  but not in People.txt")
     with open(fname, "w") as txt:
-        print("People who are scheduled but  but not in People:", file=txt)
+        print("People who are scheduled but not in People:", file=txt)
         print("(Note that these may be due to spelling differences, use of initials, etc.)", file=txt)
         count=0
         for personname in gSchedules.keys():
