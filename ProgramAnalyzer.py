cd21b654aeaad7ed27fd5726a7b7b3df0d90f01d
mlo31415 <mlo@baskerville.org>
2026-05-01 07:31:46 -0400

    REname for clarity

--- ProgramAnalyzer.py
+++ ProgramAnalyzer.py
@@ -1006,12 +1006,12 @@ def ReadSheetFromXLSXTab(workbook: openpyxl.Workbook, parms: ParmDict, parmname:
     rows=[row for row in rows if any([cell != "" for cell in row])]         # Eliminate entirely empty rows
     rows=[[str(cell) for cell in row] for row in rows]              # Some cells seem to come through as ints -- turn them into strs
     rows=[row for row in rows if "".join(row)[0] != "#"]            # Ignore rows where the 1st character is a "#"
-    trimmed=[]      # Remove trailing empty cells (Probably not needed, but better duplicates what Googledocs returns.)
+    trimmedRows=[]      # Remove trailing empty cells (Probably not needed, but better duplicates what Googledocs returns.)
     for row in rows:
         while row[-1] == "":
             row.pop()
-        trimmed.append(row)
-    return trimmed
+        trimmedRows.append(row)
+    return trimmedRows
 
 
 def PersonOfInterest(personname: str, gschedules: dict[str, list[ScheduleElement]]) -> bool:
